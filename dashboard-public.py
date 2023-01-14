a40b8af557f5bde4760a615b4070105d6da43b88
joey-van-alphen2 <120799916+joey-van-alphen2@users.noreply.github.com>
2023-01-14 23:42:16 +0100

    Update dashboard-public.py

--- dashboard-public.py
+++ dashboard-public.py
@@ -179,7 +179,7 @@ def main():
     
     
     col1, col2 = st.columns(2)
-    time_period = col1.selectbox("Select time period:", ["Total", "Monthly", "Weekly", "Yearly"], default='Total')
+    time_period = col1.selectbox("Select time period:", ["Total", "Monthly", "Weekly", "Yearly"], key='Total')
     if time_period == "Total":
         kpi1.metric(
         label="Totaal verbruik 🔥",
