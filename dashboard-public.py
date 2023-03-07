d26bb7f258740f32d482f895c5ffc757d7592491
joey-van-alphen2 <120799916+joey-van-alphen2@users.noreply.github.com>
2023-03-07 16:57:26 +0100

    Update dashboard-public.py

--- dashboard-public.py
+++ dashboard-public.py
@@ -28,7 +28,7 @@ def main():
     menu_items={
         'Get Help': 'mailto:joey0405@hotmail.com',
         'Report a bug': "mailto:joey0405@hotmail.com",
-        'About': "This is a header. This is an *extremely* cool app!"
+        'About': "Dashboard voor stadsverwarming en warm tap water vebruik"
     }
 )
     
